e6ae13d19b7e9070e6848c72a160734395f865e7
Omar Carey <omarcarey@omars-mbp-2.lan>
2024-05-06 21:10:15 -0500

    move dependency versions to root toml and read rutnime wasm binary

--- crates/node/src/commands/start.rs
+++ crates/node/src/commands/start.rs
@@ -1,11 +1,18 @@
-// use runtime::WASM_BINARY;
-// println!("Binary: {:?}", WASM_BINARY);
+use runtime::{RuntimeGenesisConfig, WASM_BINARY};
+use sc_service::GenericChainSpec;
 
 #[derive(Debug, clap::Args)]
 pub struct StartCmd {}
 
 impl StartCmd {
     pub fn run(&self) {
-        println!("inside start commmand");
+        let _ = load_spec();
     }
+}
+
+fn load_spec() {
+    let code = WASM_BINARY
+        .ok_or_else(|| "wasm binary not available".to_string())
+        .unwrap();
+    let _foo: GenericChainSpec<RuntimeGenesisConfig> = GenericChainSpec::builder(code, None).build();
 }
