66e80a99c6b1a185d028846d7665180b18a12d62
Omar Carey <omarcarey@Omars-MacBook-Pro-2.local>
2024-05-17 04:56:31 -0500

    successfully instantiate contract

--- crates/foo/src/main.rs
+++ crates/foo/src/main.rs
@@ -1,27 +1,43 @@
-use subxt::{Error, OnlineClient, SubstrateConfig};
+use subxt::{storage::StorageAddress, Error, OnlineClient, SubstrateConfig};
 use subxt_signer::sr25519;
 
 #[subxt::subxt(runtime_metadata_path = "../../chain.scale")]
 pub mod chain {}
 
-use chain::{contracts::events::CodeStored, runtime_types::{pallet_contracts::wasm::Determinism, sp_weights::weight_v2::Weight}};
+use chain::{
+    contracts::events::{CodeStored, Instantiated},
+    runtime_types::sp_weights::weight_v2::Weight,
+    runtime_apis::contracts_api::types::upload_code::Determinism
+};
 
-use tokio::fs::File;
-use tokio::io::AsyncReadExt;
+use subxt::ext::futures::StreamExt;
+use subxt::storage::address::Address;
+
+// use tokio::{fs::File, io::AsyncReadExt};
 
 const PROOF_SIZE: u64 = u64::MAX / 2;
 
+const CONTRACT: &str = r#"
+    (module
+        (import "env" "memory" (memory 1 1))
+        (func (export "deploy"))
+        (func (export "call"))
+    )
+"#;
+
 #[tokio::main]
 async fn main() {
-    // look into using chain::tx().instantiate_with_code() instead of doing upload_code
+
+    // let contract_wasm = read_wasm().await;
+    let code = wabt::wat2wasm(CONTRACT).expect("invalid wabt");
+
     let client = OnlineClient::<SubstrateConfig>::new().await.unwrap();
 
-    let contract_wasm = read_wasm().await;
     let alice = sr25519::dev::alice();
 
     // let upload_tx = chain::tx()
     //     .contracts()
-    //     .upload_code(contract_wasm, None, Determinism::Enforced);
+    //     .upload_code(code, None, Determinism::Enforced);
 
     let instantiate_tx = chain::tx().contracts().instantiate_with_code(
         100_000_000_000_000_000, // endowment
@@ -30,9 +46,9 @@ async fn main() {
             proof_size: PROOF_SIZE,
         }, // gas_limit
         None,
-        contract_wasm,
+        code,
         vec![], // data
-        vec![], // salt
+        vec![4], // salt
     );
 
     let signed_extrinsic = client
@@ -41,44 +57,31 @@ async fn main() {
         .await
         .unwrap();
 
-    let upload_result = signed_extrinsic
+    let events = signed_extrinsic
         .submit_and_watch()
         .await
         .unwrap()
-        .wait_for_finalized()
+        .wait_for_finalized_success()
         .await
         .unwrap();
 
-    println!("{:?}", upload_result);
 
-    // let code_stored = events
-    //     .find_first::<CodeStored>()
-    //     .unwrap()
-    //     .ok_or_else(|| Error::Other("Failed to find a CodeStored event".into()))
-    //     .unwrap();
+    let instantiated = events
+        .find_first::<Instantiated>()
+        .unwrap()
+        .ok_or_else(|| Error::Other("Failed to find a Instantiated event".into())).unwrap();
 
-    // println!("Code stored {:?}", code_stored);
 
-    // let events = client.events().at_latest().await.unwrap();
 
-    // for event in events.iter() {
-    //     let event = event.unwrap();
-
-    //     let pallet = event.pallet_name();
-    //     let variant = event.variant_name();
-    //     let field_values = event.field_values().unwrap();
-
-    //     println!("{pallet}::{variant}: {field_values}");
-    // }
 }
 
-async fn read_wasm() -> Vec<u8> {
-    let mut file = File::open("./target/ink/contract/contract.wasm")
-        .await
-        .unwrap();
+// async fn read_wasm() -> Vec<u8> {
+//     let mut file = File::open("./target/ink/contract/contract.wasm")
+//         .await
+//         .unwrap();
 
-    let mut contents = vec![];
-    file.read_to_end(&mut contents).await.unwrap();
+//     let mut contents = vec![];
+//     file.read_to_end(&mut contents).await.unwrap();
 
-    contents
-}
+//     contents
+// }
