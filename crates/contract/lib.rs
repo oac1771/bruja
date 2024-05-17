f9d74cd31053eadf66675af9c4a6c966daf8cae7
Omar Carey <omarcarey@Omars-MacBook-Pro-2.local>
2024-05-17 09:52:12 -0700

    add deploying contract to ink test

--- crates/contract/lib.rs
+++ crates/contract/lib.rs
@@ -44,21 +44,73 @@ mod catalog {
     // #[cfg(all(test, feature = "e2e-tests"))]
     #[cfg(test)]
     mod e2e_tests {
-        use super::*;
-        use subxt::{OnlineClient, SubstrateConfig};
+        // use super::*;
+        use subxt::{utils::{AccountId32, MultiAddress}, Error, OnlineClient, SubstrateConfig};
+        use subxt_signer::sr25519;
+        use rand::Rng;
 
         #[subxt::subxt(runtime_metadata_path = "../../chain.scale")]
         pub mod chain {}
 
+        use chain::{contracts::events::Instantiated, runtime_types::sp_weights::weight_v2::Weight};
+
+        const PROOF_SIZE: u64 = u64::MAX / 2;
+
+        const CONTRACT: &str = r#"
+            (module
+                (import "env" "memory" (memory 1 1))
+                (func (export "deploy"))
+                (func (export "call"))
+            )
+        "#;
+
+        async fn deploy_contract() -> AccountId32 {
+            let code = wabt::wat2wasm(CONTRACT).expect("invalid wabt");
+            let alice = sr25519::dev::alice();
+            let salt: u8 = rand::thread_rng().gen();
+        
+            let client = OnlineClient::<SubstrateConfig>::new().await.unwrap();
+        
+            let instantiate_tx = chain::tx().contracts().instantiate_with_code(
+                0,
+                Weight {
+                    ref_time: 500_000_000_000,
+                    proof_size: PROOF_SIZE,
+                },
+                None,
+                code,
+                vec![],
+                vec![salt],
+            );
+        
+            let signed_extrinsic = client
+                .tx()
+                .create_signed(&instantiate_tx, &alice, Default::default())
+                .await
+                .unwrap();
+        
+            let events = signed_extrinsic
+                .submit_and_watch()
+                .await
+                .unwrap()
+                .wait_for_finalized_success()
+                .await
+                .unwrap();
+        
+            let instantiated = events
+                .find_first::<Instantiated>()
+                .unwrap()
+                .ok_or_else(|| Error::Other("Failed to find a Instantiated event".into()))
+                .unwrap();
+
+            instantiated.contract
+        }
+
         #[tokio::test]
         async fn default_works() {
-            let _foo = chain::tx();
+            let contract_id = deploy_contract().await;
 
-            let bar = CatalogRef::new();
-
-            let _client = OnlineClient::<SubstrateConfig>::new().await.unwrap();
-
-            assert!(false);
+            assert_ne!(contract_id.0.len(), 0)
         }
     }
 }
