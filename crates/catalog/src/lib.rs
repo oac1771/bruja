1892ef9909c63584ac5689d9a286606d1b17e23b
Omar Carey <omarcarey@omars-mbp-2.lan>
2024-08-11 22:30:11 -0500

    use contract-extrinsic instead of custom logic

--- crates/catalog/src/lib.rs
+++ crates/catalog/src/lib.rs
@@ -46,7 +46,7 @@ pub mod catalog {
             let caller = self.env().caller();
             self.workers.insert(caller, &val);
 
-            // so it can be checked by 3rd party
+            // add event emission 
 
             true
         }
@@ -60,100 +60,6 @@ pub mod catalog {
         fn default_works() {
             let catalog = Catalog::default();
             assert_eq!(catalog.get_worker(), Err(CatalogError::WorkerNotFound));
-        }
-    }
-
-    // #[cfg(all(test, feature = "e2e-tests"))]
-    #[cfg(test)]
-    mod e2e_tests {
-        use rand::Rng;
-        use subxt::{utils::AccountId32, Error, OnlineClient, SubstrateConfig};
-        use subxt_signer::sr25519::{dev, Keypair};
-
-        #[subxt::subxt(runtime_metadata_path = "../../chain.scale")]
-        pub mod chain {}
-
-        use chain::{
-            contracts::events::Instantiated, runtime_types::sp_weights::weight_v2::Weight,
-        };
-
-        const PROOF_SIZE: u64 = u64::MAX / 2;
-
-        const CONTRACT: &str = r#"
-            (module
-                (import "env" "memory" (memory 1 1))
-                (func (export "deploy"))
-                (func (export "call"))
-            )
-        "#;
-
-        struct TestCtx {
-            client: OnlineClient<SubstrateConfig>,
-            signer: Keypair,
-        }
-
-        impl TestCtx {
-            async fn new(signer: Keypair) -> Self {
-                Self {
-                    client: OnlineClient::<SubstrateConfig>::new().await.unwrap(),
-                    signer,
-                }
-            }
-
-            async fn deploy_contract(&self) -> AccountId32 {
-                let code = wabt::wat2wasm(CONTRACT).expect("invalid wabt");
-
-                let salt: u8 = rand::thread_rng().gen();
-
-                let instantiate_tx = chain::tx().contracts().instantiate_with_code(
-                    0,
-                    Weight {
-                        ref_time: 500_000_000_000,
-                        proof_size: PROOF_SIZE,
-                    },
-                    None,
-                    code,
-                    vec![],
-                    vec![salt],
-                );
-
-                let signed_extrinsic = self
-                    .client
-                    .tx()
-                    .create_signed(&instantiate_tx, &self.signer, Default::default())
-                    .await
-                    .unwrap();
-
-                let events = signed_extrinsic
-                    .submit_and_watch()
-                    .await
-                    .unwrap()
-                    .wait_for_finalized_success()
-                    .await
-                    .unwrap();
-
-                let instantiated = events
-                    .find_first::<Instantiated>()
-                    .unwrap()
-                    .ok_or_else(|| Error::Other("Failed to find a Instantiated event".into()))
-                    .unwrap();
-
-                instantiated.contract
-            }
-        }
-
-        #[tokio::test]
-        async fn default_works() {
-            let contract_id = TestCtx::new(dev::alice()).await.deploy_contract().await;
-
-            assert_ne!(contract_id.0.len(), 0)
-        }
-
-        #[tokio::test]
-        async fn default_works_2() {
-            let contract_id = TestCtx::new(dev::bob()).await.deploy_contract().await;
-
-            assert_ne!(contract_id.0.len(), 0)
         }
     }
 }
--- crates/foo/src/main.rs
+++ crates/foo/src/main.rs
@@ -1,44 +1,23 @@
-mod primitives;
-
 use contract_transcode::{ink_metadata::InkProject, ContractMessageTranscoder};
-use primitives::ContractExecResult;
-use rand::Rng;
-use scale::{Decode, Encode};
-use subxt::{
-    backend::{legacy::LegacyRpcMethods, rpc::RpcClient},
-    utils::{AccountId32, MultiAddress},
-    Error, OnlineClient, SubstrateConfig,
-};
+// use contract_extrinsics::pallet_contracts_primitives::ContractExecResult;
+// use rand::Rng;
+
 use subxt_signer::sr25519::{self, Keypair};
 
-#[subxt::subxt(runtime_metadata_path = "../../chain.scale")]
-pub mod chain {}
-use chain::{contracts::events::Instantiated, runtime_types::sp_weights::weight_v2::Weight};
-
-const PROOF_SIZE: u64 = u64::MAX / 2;
-
-#[derive(Encode)]
-struct CallRequest<AccountId, Balance> {
-    origin: AccountId,
-    dest: AccountId,
-    value: Balance,
-    gas_limit: Option<Weight>,
-    storage_deposit_limit: Option<Balance>,
-    input_data: Vec<u8>,
-}
+// const PROOF_SIZE: u64 = u64::MAX / 2;
 
 #[tokio::main]
 async fn main() {
-    let transcoder = load_abi();
-    let client = OnlineClient::<SubstrateConfig>::new().await.unwrap();
+    let transcoder = load_transcoder();
+    // let client = OnlineClient::<SubstrateConfig>::new().await.unwrap();
     let signer = sr25519::dev::alice();
 
-    let address = deploy_contract(transcoder.metadata(), &client, &signer).await;
-    get_worker(&transcoder, &address, &signer).await;
+    deploy_contract(transcoder.metadata(), &signer).await;
+    // get_worker(&transcoder, &address, &signer).await;
     // set_worker(&contract, &address, &client);
 }
 
-fn load_abi() -> ContractMessageTranscoder {
+fn load_transcoder() -> ContractMessageTranscoder {
     let metadata_file = std::fs::File::open("./target/ink/catalog/catalog.json").unwrap();
     let abi: InkProject = serde_json::from_reader(metadata_file).unwrap();
 
@@ -50,113 +29,61 @@ fn load_abi() -> ContractMessageTranscoder {
     transcoder
 }
 
-fn read_wasm() -> Vec<u8> {
-    let path = "./target/ink/catalog/catalog.wasm";
-    let file = std::fs::read(path).unwrap();
+// fn read_wasm() -> Vec<u8> {
+//     let path = "./target/ink/catalog/catalog.wasm";
+//     let file = std::fs::read(path).unwrap();
 
-    file
-}
+//     file
+// }
 
-async fn deploy_contract(
-    contract: &InkProject,
-    client: &OnlineClient<SubstrateConfig>,
-    signer: &Keypair,
-) -> AccountId32 {
-    let code = read_wasm();
-    let label = "new";
-    let selector_bytes = contract
-        .spec()
-        .constructors()
-        .iter()
-        .find(|val| val.label() == label)
-        .unwrap()
-        .selector()
-        .to_bytes()
-        .to_vec();
+async fn deploy_contract(_contract: &InkProject, _signer: &Keypair) {}
 
-    let salt: u8 = rand::thread_rng().gen();
+// async fn get_worker(
+//     transcoder: &ContractMessageTranscoder,
+//     address: &AccountId32,
+//     signer: &Keypair,
+// ) {
+//     let function = "ContractsApi_call";
+//     let label = "get_worker";
+//     let rpc: LegacyRpcMethods<SubstrateConfig> =
+//         LegacyRpcMethods::new(RpcClient::from_url("ws://127.0.0.1:9944").await.unwrap());
 
-    let instantiate_tx = chain::tx().contracts().instantiate_with_code(
-        0,
-        Weight {
-            ref_time: 500_000_000_000,
-            proof_size: PROOF_SIZE,
-        },
-        None,
-        code,
-        selector_bytes,
-        vec![salt],
-    );
+//     let call_data = transcoder
+//         .metadata()
+//         .spec()
+//         .messages()
+//         .iter()
+//         .find(|msg| msg.label() == label)
+//         .unwrap()
+//         .selector()
+//         .to_bytes()
+//         .to_vec();
 
-    let signed_extrinsic = client
-        .tx()
-        .create_signed(&instantiate_tx, signer, Default::default())
-        .await
-        .unwrap();
+//     let value: u128 = 0;
 
-    let events = signed_extrinsic
-        .submit_and_watch()
-        .await
-        .unwrap()
-        .wait_for_finalized_success()
-        .await
-        .unwrap();
+//     let call_request = CallRequest {
+//         origin: signer.public_key().to_account_id(),
+//         dest: address.clone(),
+//         value: value,
+//         gas_limit: None,
+//         storage_deposit_limit: None,
+//         input_data: call_data,
+//     };
 
-    let instantiated = events
-        .find_first::<Instantiated>()
-        .unwrap()
-        .ok_or_else(|| Error::Other("Failed to find a Instantiated event".into()))
-        .unwrap();
+//     let args = call_request.encode();
 
-    instantiated.contract
-}
+//     let response = rpc.state_call(function, Some(&args), None).await.unwrap();
+//     let result: ContractExecResult<u128> =
+//         ContractExecResult::decode(&mut response.as_slice()).unwrap();
 
-async fn get_worker(
-    transcoder: &ContractMessageTranscoder,
-    address: &AccountId32,
-    signer: &Keypair,
-) {
-    let function = "ContractsApi_call";
-    let label = "get_worker";
-    let rpc: LegacyRpcMethods<SubstrateConfig> =
-        LegacyRpcMethods::new(RpcClient::from_url("ws://127.0.0.1:9944").await.unwrap());
-
-    let call_data = transcoder
-        .metadata()
-        .spec()
-        .messages()
-        .iter()
-        .find(|msg| msg.label() == label)
-        .unwrap()
-        .selector()
-        .to_bytes()
-        .to_vec();
-
-    let value: u128 = 0;
-
-    let call_request = CallRequest {
-        origin: signer.public_key().to_account_id(),
-        dest: address.clone(),
-        value: value,
-        gas_limit: None,
-        storage_deposit_limit: None,
-        input_data: call_data,
-    };
-
-    let args = call_request.encode();
-
-    let response = rpc.state_call(function, Some(&args), None).await.unwrap();
-    let result: ContractExecResult<u128> =
-        ContractExecResult::decode(&mut response.as_slice()).unwrap();
-
-    if let Ok(val) = result.result {
-        let foo = transcoder
-            .decode_message_return(label, &mut val.data.as_slice())
-            .unwrap();
-        println!("$$$ {:?}", foo);
-        println!(">>> {:?}", format!("{}", foo));
-    }
-}
+//     if let Ok(val) = result.result {
+//         let foo = transcoder
+//             .decode_message_return(label, &mut val.data.as_slice())
+//             .unwrap();
+//         println!("$$$ {:?}", foo);
+//         println!(">>> {:?}", format!("{}", foo));
+//     }
+// }
 
 // async fn _set_worker(
 //     contract: &InkProject,
