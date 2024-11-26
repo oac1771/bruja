3b0eeb57abf3a30805a9981fd18056ac6f2b47fe
Omar Carey <oac1771@gmail.com>
2024-11-25 20:33:24 -0800

    use to string()

--- crates/clis/requester/src/config.rs
+++ crates/clis/requester/src/config.rs
@@ -10,9 +10,9 @@ pub struct Config {
 
 impl Default for Config {
     fn default() -> Self {
-        let suri = var("SURI").unwrap_or(String::from("//Alice"));
+        let suri = var("SURI").unwrap_or("//Alice".to_string());
         let artifact_file_path = var("ARTIFACT_FILE_PATH")
-            .unwrap_or(String::from("./target/ink/catalog/catalog.contract"));
+            .unwrap_or("./target/ink/catalog/catalog.contract".to_string());
 
         let signer = Keypair::from_uri(&SecretUri::from_str(&suri).unwrap()).unwrap();
         let url = var("URL").unwrap_or("ws://127.0.0.1:9944".to_string());
